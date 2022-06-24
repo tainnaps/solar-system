e0bf3e8cb9b638866fcf08ae0101f5cf52823bf4
Tainá Santana <tainnaps@gmail.com>
2022-06-24 19:03:58 -0300

    style: organize Header with folder and add styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.css';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
+      <header className="solar-system-header">
         <h1>Sistema Solar</h1>
       </header>
     );
