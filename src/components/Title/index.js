476739faccdc024915b3e5f9d5bbb14950943369
Tainá Santana <tainnaps@gmail.com>
2022-06-24 19:03:34 -0300

    style: organize Title with folder and add styles

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
 
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
     return (
-      <h2>{headline}</h2>
+      <h2 className="title">{headline}</h2>
     );
   }
 }
