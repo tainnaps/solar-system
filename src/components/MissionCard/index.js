7935b24960bc295867f4c90f5c7a41a4e12bfbae
Tainá Santana <tainnaps@gmail.com>
2022-06-24 19:05:17 -0300

    style: organize MissionCard with folder and add styles

--- src/components/MissionCard/index.js
+++ src/components/MissionCard/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
 
 class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
+      <div data-testid="mission-card" className="mission-card">
         <h3 data-testid="mission-name">{name}</h3>
         <p data-testid="mission-year">{year}</p>
         <p data-testid="mission-country">{country}</p>
